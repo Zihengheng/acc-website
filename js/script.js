9b12f2f986b85d523bf48ce5a7dff4c45934bef9
wxh <zwang252325@gmail.com>
2020-12-21 09:57:54 +0800

    update

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ $( document ).ready(function() {
     $('#inbox2-tab li:eq(0) a').tab('show');
     $('#inbox3-tab li:eq(0) a').tab('show');
     $('#rsk-tab a[href="#coso"]').tab('show');
+    $('#org-nav a[href="#gltz"]').tab('show');
  
 });
