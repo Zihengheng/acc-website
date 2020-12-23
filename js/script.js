9d5bb86f19415055f9cd527528f5d611626c9293
wxh <zwang252325@gmail.com>
2020-12-23 08:35:47 +0800

    update footer

--- js/script.js
+++ js/script.js
@@ -10,159 +10,84 @@ $( document ).ready(function() {
 });
 //图书轮播
 $(function(){
+  var $nextBtn = $('img .go-left');
+  var $preBtn = $('img .go-right');
+  var $imgContent = $('ul.book-list');
+  var $img = $('ul.book-list li');
+  var imgWidth = $img.width();
+  var imgCount = $img.length;
+  var $bullets = $('.bullet>li');
 
-    // $.tab(".area-left", ".nav-list li", "cur", ".con-list", "active", "mousedown");
+  var pageIndex = 0;
 
-    //首页 研究机构轮播
-    var videoScroll1 = new LScroll();
-    videoScroll1.dom = $("#books");
-    videoScroll1.speed = 5000;
-    $("img .go-left").click(function () { videoScroll1.prev(); });
-    $("img .go-right").click(function () { videoScroll1.next(); });
-    videoScroll1.tabmarq(0, 0, 1);
+  var isAnimate = false;
 
+  // 将第一张图片克隆到最后
+  $imgContent.append($img.first().clone());
+  // 将最后一张图片克隆到最前
+  $imgContent.prepend($img.last().clone());
+
+  // 动态设置img-content容器的宽度
+  $imgContent.width((imgCount + 2) * imgWidth);
+
+  // 将原始照片放置在正确位置
+  $imgContent.css({ left: -imgWidth });
+
+  $nextBtn.click(function() {
+    playNext(1);
+  });
+
+  $preBtn.click(function() {
+    playPre(1);
+  });
+
+  $bullets.click(function() {
+    $this = $(this);
+    var bulletIndex = $this.index();
+    if (bulletIndex > pageIndex) {
+      playNext(bulletIndex - pageIndex);
+    } else if (bulletIndex < pageIndex) {
+      playPre(pageIndex - bulletIndex);
+    }
+    clearInterval(auto);
+  });
+
+  function playNext(len) {
+    if (isAnimate) return;
+    isAnimate = true;
+    $imgContent.animate({
+      left: '-=' + len * imgWidth
+    }, function() {
+      pageIndex += len;
+      if (pageIndex == imgCount) {
+        pageIndex = 0;
+        $imgContent.css({ left: -imgWidth }).fadeIn('slow');
+      }
+      setBullet();
+
+      isAnimate = false;
+    });
+  }
+
+  function playPre(len) {
+    $imgContent.animate({
+      left: '+=' + len * imgWidth
+    }, function() {
+      pageIndex -= len;
+      if (pageIndex < 0) {
+        pageIndex = imgCount - 1;
+        $imgContent.css({ left: -imgWidth * imgCount });
+      }
+      setBullet();
+    });
+  }
+
+  function setBullet() {
+    $bullets.removeClass('active').eq(pageIndex).addClass('active');
+  }
+
+  // 设置自动轮播
+    autoPlay =   setInterval(function() {
+    playNext(1);
+  }, 2000);
 });
-
-
-/*--轮播 start--*/
-var LScroll = function () {
-    var LScroll = function () {
-        return LScroll.fn.init();
-    };
-    LScroll.fn = LScroll.prototype = {
-        init: function () {
-            return this;
-        },
-        speed: 100,
-        pwidth: 1000,
-        positive: true,
-        dom: null
-    };
-    var timer, isAct = false, maxlen = 0, curpose, curno = 0, anim;
-
-    function scrollInit(mode) {
-        curpose = LScroll.fn.positive;
-        if (mode == 1) {
-            LScroll.fn.dom.find("li").each(function () {
-                maxlen += $(this).height();
-            });
-            LScroll.fn.dom.css("height", maxlen);
-        }
-        else {
-            LScroll.fn.dom.find("li").each(function () {
-                maxlen += $(this).width();
-            });
-            LScroll.fn.dom.css("width", maxlen);
-        }
-    }
-
-    function move() {
-        LScroll.fn.dom.hover(function () {
-            stop();
-        }, function () {
-            curpose = LScroll.fn.positive;
-            start();
-        });
-    }
-
-    function start() {
-        if (isAct) return;
-        isAct = true;
-        timer = setInterval(anim, LScroll.fn.speed);
-    }
-
-    function stop() {
-        isAct = false;
-        clearInterval(timer);
-    }
-
-    LScroll.fn.prev = function () {
-        stop();
-        curpose = true;
-        anim();
-        start();
-    };
-    LScroll.fn.next = function () {
-        stop();
-        curpose = false;
-        anim();
-        start();
-        curpose = true;
-    }; //此处将滑动顺序恢复默认
-    LScroll.fn.reset = function () {
-        stop();
-        LScroll.fn.dom.unbind("mouseenter").unbind("mouseleave");
-    };
-    //轮播图效果，无缝滚动 mode-0:左右，1：上下 startno-置为0，实现无缝 tabnum-每次滚动个数
-    LScroll.fn.tabmarq = function (mode, startno, tabnum) {
-        tabnum = tabnum == null ? 1 : tabnum;
-        startno = startno == null ? 0 : startno;
-        scrollInit(mode);
-        anim = function () {
-            if (curpose) { //先移动动画，结束后将第一个元素移动到最后
-                mode == 1 ? LScroll.fn.dom.animate({ top: 0 - $(LScroll.fn.dom.find("li").get(startno)).height() * tabnum }, "slow", function () {
-                    LScroll.fn.dom.append(LScroll.fn.dom.find("li").slice(startno, startno + tabnum));
-                    LScroll.fn.dom.css({ top: 0 });
-                }) : LScroll.fn.dom.animate({ left: 0 - $(LScroll.fn.dom.find("li").get(startno)).width() * tabnum }, "slow", function () {
-                    LScroll.fn.dom.append(LScroll.fn.dom.find("li").slice(startno, startno + tabnum));
-                    LScroll.fn.dom.css({ left: 0 });
-                });
-            }
-            else { //先将最后一个元素移动到最前面，然后移动动画
-                $(LScroll.fn.dom.find("li").get(startno)).before(LScroll.fn.dom.find("li").slice(LScroll.fn.dom.find("li").length - tabnum, LScroll.fn.dom.find("li").length));
-                mode == 1 ? LScroll.fn.dom.css({ top: 0 - $(LScroll.fn.dom.find("li").get(startno)).height() * tabnum }) : LScroll.fn.dom.css({ left: 0 - $(LScroll.fn.dom.find("li").get(startno)).width() * tabnum });
-                mode == 1 ? LScroll.fn.dom.animate({ top: 0 }, "slow") : LScroll.fn.dom.animate({ left: 0 }, "slow");
-            }
-        };
-        isAct = true;
-        timer = setInterval(anim, this.speed);
-        move();
-    };
-    //跑马灯效果，不能实现无缝滚动 mode-0:左右，1：上下 plen-每次移动像素
-    LScroll.fn.marquee = function (mode, plen) {
-        scrollInit(mode);
-        anim = function () {
-            if (mode == 1) {
-                var wdt = LScroll.fn.dom.position().top;
-                //wdt + maxlen < 0 && (wdt = plen);
-                LScroll.fn.dom.css("top", wdt - plen);
-            }
-            else {
-                var wdt = LScroll.fn.dom.position().left;
-                //wdt + maxlen < 0 && (wdt = plen);
-                LScroll.fn.dom.css("left", wdt - plen);
-            }
-        };
-        isAct = true;
-        timer = setInterval(anim, this.speed);
-        move();
-    };
-    LScroll.fn.fade = function (startno, numdom) {
-        curno = startno = startno == null ? 0 : startno;
-        curno++;
-        scrollInit(1);
-        if (numdom && numdom.length) {
-            numdom.hover(function () { LScroll.fn.reset(); }, function () { move(); });
-            numdom.find("li").click(function () {
-                curno = $(this).index();
-                anim();
-            });
-        }
-        anim = function () {
-            curno = curno < LScroll.fn.dom.find("li").length - startno ? curno : startno;
-            if (numdom && numdom.length) {
-                numdom.find(".on").removeClass("on");
-                numdom.find("li").eq(curno).addClass("on");
-            }
-            LScroll.fn.dom.fadeOut(500, function () {
-                LScroll.fn.dom.css({ top: 0 - $(LScroll.fn.dom.find("li").get(startno)).height() * curno });
-                curno++;
-            }).fadeIn(1000);
-        };
-        isAct = true;
-        timer = setInterval(anim, this.speed);
-        move();
-    };
-    return LScroll();
-};
-/*--轮播 end--*/
